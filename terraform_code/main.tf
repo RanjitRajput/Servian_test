98d02cfe01da3dd49d7528fef5aac0fa7029230c
RanjitRajput <ranjitcrajput@gmail.com>
2022-08-28 15:36:18 +0530

    adding ecs task defination

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -12,3 +12,40 @@ resource "aws_ecs_cluster" "cluster" {
     value = "disabled"
   }
 }
+
+module "ecs-fargate" {
+  source  = "umotif-public/ecs-fargate/aws"
+  version = "~> 6.1.0"
+
+  name_prefix        = "ecs-fargate-servian"
+  vpc_id             = aws_vpc.ecs_vpc.id
+  private_subnet_ids = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+
+  cluster_id = aws_ecs_cluster.cluster.id
+
+  task_container_image   = "servian/techchallengeapp:latest"
+  task_definition_cpu    = 256
+  task_definition_memory = 512
+
+  task_container_port             = 80
+  task_container_assign_public_ip = true
+
+  load_balanced = false
+
+  target_groups = [
+    {
+      target_group_name = "tg-fargate-servian"
+      container_port    = 80
+    }
+  ]
+
+  health_check = {
+    port = "traffic-port"
+    path = "/healthcheck/"
+  }
+
+  tags = {
+    Environment = "test"
+    Project     = "Test"
+  }
+}
